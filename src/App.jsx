c37010f5b4957800e18f538cfc481a16241f3dd4
baistan.tashkuov <baistan.xi@gmail.com>
2023-01-30 17:10:34 +0600

    [change] add site name

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,16 @@ function App() {
   return (
     <>
       <Container maxWidth="lg" sx={{ paddingTop: "4rem" }}>
-        <h1 className="font-bold text-4xl mb-10 text-center">Analytics</h1>
+        <h1 className="font-bold text-4xl mb-10 text-center">
+          Analytics of{" "}
+          <a
+            className="text-blue-500 underline"
+            href="https://galactictapestry.com/"
+            target="_blank"
+          >
+            Galactictapestry.com
+          </a>{" "}
+        </h1>
         <TableExample />
       </Container>
     </>
