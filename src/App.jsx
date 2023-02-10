f8fe506dce4d72ccbb1ab5e778ea924bef805923
baistan.tashkuov <baistan.xi@gmail.com>
2023-02-11 00:31:35 +0600

    add lp tracking

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,7 @@ import NewTable from "./components/NewTable";
 function App() {
   return (
     <div className="p-4">
-      <h1 className="font-bold text-4xl mb-10 text-center">
-        Analytics of{" "}
-        <a
-          className="text-blue-500 underline"
-          href="https://galactictapestry.com/"
-          target="_blank"
-        >
-          Galactictapestry.com
-        </a>{" "}
-      </h1>
+      <h1 className="font-bold text-4xl mb-4 text-center">Analytics</h1>
       {/* <TableExample /> */}
       <NewTable />
     </div>
--- src/components/NewTable.jsx
+++ src/components/NewTable.jsx
@@ -4,13 +4,19 @@ import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 const NewTable = () => {
   const [data, setData] = useState([]);
+  const [store, setStore] = useState([]);
   const [loading, setLoading] = useState(false);
   const [sortField, setSortField] = useState("");
   const [order, setOrder] = useState("asc");
+  const [selectedId, setSelectedId] = useState(1);
+  const [collapse, setCollapse] = useState(false);
 
-  const getData = () => {
+  const getData = (id) => {
+    if (!id) {
+      id = 1;
+    }
     setLoading(true);
-    fetch("https://narynkey.pythonanywhere.com/api/getproducts/1")
+    fetch(`https://narynkey.pythonanywhere.com/api/getproducts/${id}`)
       .then((res) => res.json())
       .then((res) => {
         setData(res);
@@ -20,9 +26,26 @@ const NewTable = () => {
       .finally((e) => setLoading(false));
   };
 
+  const getStore = () => {
+    fetch("https://narynkey.pythonanywhere.com/api/stores/")
+      .then((res) => res.json())
+      .then((res) => {
+        setStore(res.results);
+        console.log(res.results);
+      })
+      .catch((e) => console.log(e))
+      .finally((e) => setLoading(false));
+  };
+
   useEffect(() => {
     getData();
+    getStore();
   }, []);
+
+  const setCurrentStore = (id) => {
+    getData(id);
+    setSelectedId(id);
+  };
 
   const handleSortingChange = (accessor) => {
     const sortOrder =
@@ -52,165 +75,206 @@ const NewTable = () => {
     return "Loading";
   }
   return (
-    <table className="border border-collapse border-gray-100 m-auto">
-      <thead>
-        <tr>
-          <th rowSpan={2} className="border">
-            Product Name
-          </th>
-          <th rowSpan={2} className="border">
-            LP #
-          </th>
-          <th rowSpan={2} className="border">
-            Landing page url
-          </th>
-          <th
-            onClick={() => handleSortingChange("unique_sessions")}
-            colSpan={2}
-            className="border"
-          >
-            Unique Landing page Views
-            {order === "desc" && sortField === "unique_sessions" ? (
-              <KeyboardArrowUpIcon className="w-2 h-2" />
-            ) : (
-              <KeyboardArrowDownIcon />
-            )}
-          </th>
-          <th
-            onClick={() => handleSortingChange("checkout_step_1")}
-            colSpan={2}
-            className="border"
-          >
-            Checkout Step 1
-            {order === "desc" && sortField === "checkout_step_1" ? (
-              <KeyboardArrowUpIcon />
-            ) : (
-              <KeyboardArrowDownIcon />
-            )}
-          </th>
-          <th
-            onClick={() => handleSortingChange("checkout_step_2")}
-            colSpan={2}
-            className="border"
-          >
-            Check out step 2
-            {order === "desc" && sortField === "checkout_step_2" ? (
-              <KeyboardArrowUpIcon />
-            ) : (
-              <KeyboardArrowDownIcon />
-            )}
-          </th>
-          <th
-            onClick={() => handleSortingChange("purchases")}
-            colSpan={2}
-            className="border"
-          >
-            Purchase
-            {order === "desc" && sortField === "purchases" ? (
-              <KeyboardArrowUpIcon />
-            ) : (
-              <KeyboardArrowDownIcon />
-            )}
-          </th>
-          <th onClick={() => handleSortingChange("cvr_%")} className="border">
-            CVR
-            {order === "desc" && sortField === "cvr_%" ? (
-              <KeyboardArrowUpIcon />
-            ) : (
-              <KeyboardArrowDownIcon />
-            )}
-          </th>
-          <th
-            onClick={() => handleSortingChange("upsell_1_takes")}
-            colSpan={2}
-            className="border"
-          >
-            Upsell 1 Takes
-            {order === "desc" && sortField === "upsell_1_takes" ? (
-              <KeyboardArrowUpIcon />
-            ) : (
-              <KeyboardArrowDownIcon />
-            )}
-          </th>
-          <th
-            onClick={() => handleSortingChange("upsell_2_takes")}
-            colSpan={2}
-            className="border"
-          >
-            Upsell 2 Takes
-            {order === "desc" && sortField === "upsell_2_takes" ? (
-              <KeyboardArrowUpIcon />
-            ) : (
-              <KeyboardArrowDownIcon />
-            )}
-          </th>
-          <th
-            onClick={() => handleSortingChange("upsell_3_takes")}
-            colSpan={2}
-            className="border"
-          >
-            Upsell 3 takes
-            {order === "desc" && sortField === "upsell_3_takes" ? (
-              <KeyboardArrowUpIcon />
-            ) : (
-              <KeyboardArrowDownIcon />
-            )}
-          </th>
-        </tr>
-        <tr>
-          <th className="border">Count</th>
-          <th className="border">%</th>
-          <th className="border">Count</th>
-          <th className="border">%</th>
-          <th className="border">Count</th>
-          <th className="border">%</th>
-          <th className="border">Count</th>
-          <th className="border">%</th>
-          <th className="border">%</th>
-          <th className="border">Count</th>
-          <th className="border">%</th>
-          <th className="border">Count</th>
-          <th className="border">%</th>
-          <th className="border">Count</th>
-          <th className="border">%</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data?.map((row, idx) => (
-          <tr key={idx}>
-            <td className="border">{row.name}</td>
-            <td className="border">#1</td>
-            <td className="border">
-              <a
-                className="text-blue-500 underline"
-                href={`https://galactictapestry.com/products/${row.name.replaceAll(
-                  " ",
-                  "-"
-                )}`}
-                target="_blank"
-              >
-                Link
-              </a>
-            </td>
-            <td className="border">{row.unique_sessions}</td>
-            <td className="border">100%</td>
-            <td className="border">{row.checkout_step_1[0]}</td>
-            <td className="border">{row.checkout_step_1[1]}%</td>
-            <td className="border">{row.checkout_step_2[0]}</td>
-            <td className="border">{row.checkout_step_2[1]}%</td>
-            <td className="border">{row.purchases[0]}</td>
-            <td className="border">{row.purchases[1]}%</td>
-            <td className="border">{`${row["cvr_%"]}`}%</td>
-            <td className="border">{row.upsell_1_takes[0]}</td>
-            <td className="border">{row.upsell_1_takes[1]}%</td>
-            <td className="border">{row.upsell_2_takes[0]}</td>
-            <td className="border">{row.upsell_2_takes[1]}%</td>
-            <td className="border">{row.upsell_3_takes[0]}</td>
-            <td className="border">{row.upsell_3_takes[1]}%</td>
+    <>
+      <div className="flex justify-center items-center mb-6">
+        <select
+          defaultValue={selectedId}
+          onChange={(e) => setCurrentStore(e.target.value)}
+        >
+          {store?.map((el) => {
+            if (el.name.split(".").length <= 2) {
+              return (
+                <option key={el.id} value={el.id}>
+                  {el.name}
+                </option>
+              );
+            }
+          })}
+        </select>
+      </div>
+      <table className="border border-collapse border-gray-100 m-auto">
+        <thead>
+          <tr>
+            <th rowSpan={2} className="border">
+              Product Name
+            </th>
+            <th rowSpan={2} className="border">
+              Landing page url
+            </th>
+            <th
+              onClick={() => handleSortingChange("unique_sessions")}
+              colSpan={2}
+              className="border"
+            >
+              Unique Landing page Views
+              {order === "desc" && sortField === "unique_sessions" ? (
+                <KeyboardArrowUpIcon className="w-2 h-2" />
+              ) : (
+                <KeyboardArrowDownIcon />
+              )}
+            </th>
+            <th
+              onClick={() => handleSortingChange("checkout_step_1")}
+              colSpan={2}
+              className="border"
+            >
+              Checkout Step 1
+              {order === "desc" && sortField === "checkout_step_1" ? (
+                <KeyboardArrowUpIcon />
+              ) : (
+                <KeyboardArrowDownIcon />
+              )}
+            </th>
+            <th
+              onClick={() => handleSortingChange("checkout_step_2")}
+              colSpan={2}
+              className="border"
+            >
+              Check out step 2
+              {order === "desc" && sortField === "checkout_step_2" ? (
+                <KeyboardArrowUpIcon />
+              ) : (
+                <KeyboardArrowDownIcon />
+              )}
+            </th>
+            <th
+              onClick={() => handleSortingChange("purchases")}
+              colSpan={2}
+              className="border"
+            >
+              Purchase
+              {order === "desc" && sortField === "purchases" ? (
+                <KeyboardArrowUpIcon />
+              ) : (
+                <KeyboardArrowDownIcon />
+              )}
+            </th>
+            <th onClick={() => handleSortingChange("cvr_%")} className="border">
+              CVR
+              {order === "desc" && sortField === "cvr_%" ? (
+                <KeyboardArrowUpIcon />
+              ) : (
+                <KeyboardArrowDownIcon />
+              )}
+            </th>
+            <th
+              onClick={() => handleSortingChange("upsell_1_takes")}
+              colSpan={2}
+              className="border"
+            >
+              Upsell 1 Takes
+              {order === "desc" && sortField === "upsell_1_takes" ? (
+                <KeyboardArrowUpIcon />
+              ) : (
+                <KeyboardArrowDownIcon />
+              )}
+            </th>
+            <th
+              onClick={() => handleSortingChange("upsell_2_takes")}
+              colSpan={2}
+              className="border"
+            >
+              Upsell 2 Takes
+              {order === "desc" && sortField === "upsell_2_takes" ? (
+                <KeyboardArrowUpIcon />
+              ) : (
+                <KeyboardArrowDownIcon />
+              )}
+            </th>
+            <th
+              onClick={() => handleSortingChange("upsell_3_takes")}
+              colSpan={2}
+              className="border"
+            >
+              Upsell 3 takes
+              {order === "desc" && sortField === "upsell_3_takes" ? (
+                <KeyboardArrowUpIcon />
+              ) : (
+                <KeyboardArrowDownIcon />
+              )}
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+          <tr>
+            <th className="border">Count</th>
+            <th className="border">%</th>
+            <th className="border">Count</th>
+            <th className="border">%</th>
+            <th className="border">Count</th>
+            <th className="border">%</th>
+            <th className="border">Count</th>
+            <th className="border">%</th>
+            <th className="border">%</th>
+            <th className="border">Count</th>
+            <th className="border">%</th>
+            <th className="border">Count</th>
+            <th className="border">%</th>
+            <th className="border">Count</th>
+            <th className="border">%</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((row, idx) => (
+            <>
+              <tr
+                className="hover:bg-gray-100 cursor-pointer"
+                onClick={() => setCollapse(idx)}
+                key={idx}
+              >
+                <td className="border">{row.name}</td>
+
+                <td className="border text-gray-500">Total funnel</td>
+                <td className="border">{row.unique_sessions}</td>
+                <td className="border">100%</td>
+                <td className="border">{row.checkout_step_1[0]}</td>
+                <td className="border">{row.checkout_step_1[1]}%</td>
+                <td className="border">{row.checkout_step_2[0]}</td>
+                <td className="border">{row.checkout_step_2[1]}%</td>
+                <td className="border">{row.purchases[0]}</td>
+                <td className="border">{row.purchases[1]}%</td>
+                <td className="border">{`${row["cvr_%"]}`}%</td>
+                <td className="border">{row.upsell_1_takes[0]}</td>
+                <td className="border">{row.upsell_1_takes[1]}%</td>
+                <td className="border">{row.upsell_2_takes[0]}</td>
+                <td className="border">{row.upsell_2_takes[1]}%</td>
+                <td className="border">{row.upsell_3_takes[0]}</td>
+                <td className="border">{row.upsell_3_takes[1]}%</td>
+              </tr>
+              {collapse === idx &&
+                row.lps.map((el, idx) => (
+                  <tr className="bg-gray-50">
+                    <td></td>
+                    <td>
+                      {" "}
+                      <a
+                        className="text-blue-500 underline"
+                        href={`https://${el.lp_name}`}
+                        target="_blank"
+                      >
+                        {el.lp_name}
+                      </a>
+                    </td>
+                    <td className="border">{el.unique_sessions}</td>
+                    <td className="border">100%</td>
+                    <td className="border">{el.checkout_step_1[0]}</td>
+                    <td className="border">{el.checkout_step_1[1]}%</td>
+                    <td className="border">{el.checkout_step_2[0]}</td>
+                    <td className="border">{el.checkout_step_2[1]}%</td>
+                    <td className="border">{el.purchases[0]}</td>
+                    <td className="border">{el.purchases[1]}%</td>
+                    <td className="border">{`${el["cvr_%"]}`}%</td>
+                    <td className="border">{el.upsell_1_takes[0]}</td>
+                    <td className="border">{el.upsell_1_takes[1]}%</td>
+                    <td className="border">{el.upsell_2_takes[0]}</td>
+                    <td className="border">{el.upsell_2_takes[1]}%</td>
+                    <td className="border">{el.upsell_3_takes[0]}</td>
+                    <td className="border">{el.upsell_3_takes[1]}%</td>
+                  </tr>
+                ))}
+            </>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
 
